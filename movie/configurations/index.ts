9ceeaa63d9ed2a4119af874d437435a644242326
AshenafiDegefe <ashenafidegefe8@gmail.com>
2025-11-23 13:19:36 +0300

    create config file to store api keys movie database

---
+++ movie/configurations/index.ts
@@ -0,0 +1,5 @@
+export const MOVIE_ID = '550'; 
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w300';
+export const API_URL = `${BASE_URL}${MOVIE_ID}/recommendations?language=en-US&page=1`;
+export const TMDB_API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDY2ZGMzYTlhZjUwMzQ4ZTAyNjM2ZjNjY2U4NTVkZSIsIm5iZiI6MTc2MzU1MTQ0My45NzYsInN1YiI6IjY5MWRhOGQzMjg0ZTgxN2QxNTdhZDhiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FDL3D7HBr3uP-9zzYYYvTUkPFMcQifjgs5kOVnJ3idY';
