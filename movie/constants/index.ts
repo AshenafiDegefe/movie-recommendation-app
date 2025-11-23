954de9331ac8aa3346e0f871e0e4ef5e623066ff
AshenafiDegefe <ashenafidegefe8@gmail.com>
2025-11-23 13:04:07 +0300

    create constant component

---
+++ movie/constants/index.ts
@@ -0,0 +1,5 @@
+export  const SCROLL_CONTAINER_STYLE = `
+  .scroll-container::-webkit-scrollbar { height: 8px; }
+  .scroll-container::-webkit-scrollbar-thumb { background-color: #38bdf8; border-radius: 4px; }
+  .scroll-container::-webkit-scrollbar-track { background-color: #161b22; }
+`;
